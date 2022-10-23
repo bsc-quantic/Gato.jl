a8cc8a8de825bb6cfbb047da20e587dd53e4e834
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-10-23 14:12:29 +0200

    Refactor `State` to generalize over array backend

--- src/State.jl
+++ src/State.jl
@@ -1,49 +1,35 @@
 using Quac
-import LinearAlgebra: norm
 
 export State
-export run, apply!
+export run, apply!, data
 
 # TODO allow for physical dimensions != 2
 """
 Represents the statevector of a pure state with a dense array.
 """
-struct State{F}
-    data::AbstractArray{Complex{F}}
-    inds::Vector{Symbol}
+struct State{A<:AbstractArray}
+    data::A
+end
 
-	"""
-		State{F}(n)
+State(data::A) where {A<:AbstractArray} = State{A}(data)
 
-	Initialize `State` to $\ket{0 \dots 0}$ product state.
-	"""
-    function State{F}(n::Int)
-		data = zeros(Complex{F}, fill(2, n)...)
-		data[1] = 1.0 + 0.0im
-		new(, 1:n)
-	end
+function State{F}(n::Int) where {F<:AbstractFloat}
+    data = zeros(Complex{F}, fill(2, n)...)
+    data[1] = 1.0 + 0.0im
+
+    State(data)
 end
 
 State(n::Int) = State{Float32}(n)
 
-LinearAlgebra.norm(Ψ::State, p::Real=2) = norm(Ψ.data, p)
+data(Ψ::State) = Ψ.data
 
+eltype(::Type{State{A}}) where {A} = eltype(A)
+eltype(o::State{A}) where {A} = eltype(A)
 
-function run(circ::Circuit, Ψ::State)
-    foreach(gate -> apply!(Ψ, gate), circ)
+# TODO return a view of the `State`
+function Base.getindex(s::State, p::Base.Pairs{Int,Int})
+    error("not implemented yet")
 end
 
-function apply!(Ψ::State{F}, gate::AbstractGate) where {F<:AbstractFloat}
-    apply_matmul(Ψ, gate)
-end
-
-function apply_matmul!(Ψ::State{F}, gate) where {F<:AbstractFloat}
-    # TODO permute indices
-    perm = ...
-    permutedims!(Ψ.data, perm)
-
-    shape = size(Ψ.data)
-    Ψ.data = reshape(Ψ.data, ...)
-
-    # TODO multiply matrices
-end
+run(circ::Circuit, Ψ::State) = foreach(gate -> apply!(Ψ, gate), circ)
