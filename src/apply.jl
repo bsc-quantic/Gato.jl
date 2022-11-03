d691eb6bf7a541ff58ebfc12b7168a04878c6313
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-03 15:25:23 +0100

    Document `apply!` methods

--- src/apply.jl
+++ src/apply.jl
@@ -1,5 +1,6 @@
 using Quac
 using LinearAlgebra: Diagonal, rmul!
+import .Kernel
 
 function apply!(Ψ::State{F}, gate::AbstractGate) where {F<:AbstractFloat}
     apply_matmul!(Ψ, gate)
@@ -10,6 +11,9 @@ end
 
 apply!(Ψ::State, gate::I) = nothing
 
+@doc raw"""
+Apply the ``X`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::X)
     lane = only(lanes(gate))
     A = selectdim(data(Ψ), lane, 1)
@@ -17,61 +21,93 @@ function apply!(Ψ::State, gate::X)
     Kernel.swap!(A, B)
 end
 
-# TODO use antidiagonal representation
+@doc raw"""
+Apply the ``Y`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Y)
     error("not implemented yet")
 end
 
+@doc raw"""
+Apply the ``Z`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Z)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>2], -1)
 end
 
+@doc raw"""
+Apply the Hadamard gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::H)
     error("not implemented yet")
 end
 
+@doc raw"""
+Apply the ``S`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::S)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>2], cispi(1 // 2))
 end
 
+@doc raw"""
+Apply the ``S^\dagger`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Sd)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>2], cispi(-1 // 2))
 end
 
+@doc raw"""
+Apply the ``T`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::T)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>2], cispi(1 // 4))
 end
 
+@doc raw"""
+Apply the ``T^\dagger`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Td)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>2], cispi(-1 // 4))
 end
 
+@doc raw"""
+Apply the ``R_X`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Rx)
     error("not implemented yet")
 end
 
+@doc raw"""
+Apply the ``R_Y`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Ry)
     error("not implemented yet")
 end
 
+@doc raw"""
+Apply the ``R_Z`` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Rz)
     lane = only(lanes(gate))
     rmul!(Ψ[lane=>1], cis(x.θ))
 end
 
 # TODO check no problem if single control or multicontrol
+@doc raw"""
+Apply the `Control{T}` gate to the ``\Psi`` `State`.
+"""
 function apply!(Ψ::State, gate::Control)
     addr = [lane => 2 for lane in control(gate)]
     apply!(Ψ[addr...], gate.op)
 end
 
 @doc raw"""
-Apply the **SWAP** gate to ``\Psi`` `State`.
+Apply the ``SWAP`` gate to ``\Psi`` `State`.
 
 # Notes
 Current implementation lazily permutes the amplitude: i.e. it does not permute the data but the pointers to the data.
